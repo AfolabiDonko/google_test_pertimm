d5a4426ad297b70ed291dad8e4ba5a3fb1cdb798
DONKO Afolabi <ddonkbienv@gmail.com>
2021-02-18 23:51:46 +0100

    add inout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="q-pa-md">
-      <div class="row">
-          <q-img
-            src="google.png"
-            spinner-color="white"
-            style="height: 100%; max-width: 100%"
-            img-class="my-custom-image"
-            class="rounded-borders"
-          >
-          </q-img>
+  <div class="q-pa-md">
+    <div class="column rSk4se">
+      <div class="col">
+        <q-img
+          src="google.png"
+          spinner-color="white"
+          style="height: 92px; width: 272px; text-align: center;margin-top: 260px;margin-left: 40%;margin-right: 40%;"
+          img-class="my-custom-image"
+          class="rounded-borders"
+        />
       </div>
-
-      <div class="row">
-        <div class="q-gutter-md" style="max-width: 300px">
+    </div>
+    <div class="column k1zIA">
+      <div class="col">
+        <div class="q-gutter-md" style=" background-color: green; margin-left: 30%; margin-right: 25%; min-width : 25%">
           <q-input rounded outlined >
             <template v-slot:prepend>
               <q-icon name="search" />
@@ -24,8 +24,26 @@
           </q-input>
         </div>
       </div>
+      <div class="col">
+        <div class="row justify-evenly">
+          <div class="col-4">
+            <q-btn color="white" text-color="black" label="Recherche Google" />
+          </div>
+          <div class="col-4">
+            <q-btn color="white" text-color="black" label="J'ai de la chance" />
+          </div>
+        </div>
+      </div>
     </div>
-  </q-page>
+    <div class="column gyttt">
+      <div class="col">
+        <div>
+          Google disponible en : <a href="">English</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -35,13 +53,25 @@ export default {
 </script>
 <style scoped>
   .rSk4se {
-      max-height: 92px;
+    height: 355px;
+    display: flex;
+    background-color: red;
+    min-height: 92px;
+    max-height: 590px;
   }
   .k1zIA {
-      height: 100%;
-      margin-top: auto;
+    height: 175px;
+    display: flex;
+    flex-direction: column;
+    background-color: yellow;
+    padding: 20px;
   }
-  div {
-      display: block;
+  .gyttt {
+    height: 80px;
+    display: flex center;
+    flex-direction: column;
+    background-color: green;
+    text-align: center;
+    padding: 20px;
   }
 </style>
