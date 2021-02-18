1230004d7518164e070b0a982ecbec0513b1b9d9
DONKO Afolabi <ddonkbienv@gmail.com>
2021-02-18 22:35:56 +0100

    add google image and input

--- src/layouts/GoogleLayout.vue
+++ src/layouts/GoogleLayout.vue
@@ -1,6 +1,18 @@
 <template>
   <q-layout view="lHr lpr fFf">
-      jfdngdnj
+      <q-header reveal class="bg-black">
+        <q-toolbar>
+          <q-toolbar-title>Header</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+      <q-footer>
+        <q-toolbar>
+          <q-toolbar-title>Footer</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+      <q-page-container>
+        <router-view></router-view>
+      </q-page-container>
   </q-layout>
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,30 @@
 <template>
   <q-page class="flex flex-center">
+    <div class="q-pa-md">
+      <div class="row">
+          <q-img
+            src="google.png"
+            spinner-color="white"
+            style="height: 100%; max-width: 100%"
+            img-class="my-custom-image"
+            class="rounded-borders"
+          >
+          </q-img>
+      </div>
+
+      <div class="row">
+        <div class="q-gutter-md" style="max-width: 300px">
+          <q-input rounded outlined >
+            <template v-slot:prepend>
+              <q-icon name="search" />
+            </template>
+            <template v-slot:append>
+              <q-icon name="mic" />
+            </template>
+          </q-input>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -8,3 +33,15 @@ export default {
   name: 'GoogleLayout'
 }
 </script>
+<style scoped>
+  .rSk4se {
+      max-height: 92px;
+  }
+  .k1zIA {
+      height: 100%;
+      margin-top: auto;
+  }
+  div {
+      display: block;
+  }
+</style>
